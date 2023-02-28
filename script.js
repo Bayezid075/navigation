57886ee7c53fb4ee0f4e3304a306499eb40e0924
Bayezid075 <mrbayezidhoshen@gmail.com>
2023-02-28 22:08:29 +0600

    aniamtion

--- script.js
+++ script.js
@@ -15,10 +15,32 @@ function toggleMenuBar() {
     //Animate in
     overlay.classList.remove("slide-close");
     overlay.classList.add("slide-open");
+    // Animate in navitem
+    nav1.classList.remove("slide-out-anm-1");
+    nav1.classList.add("slide-in-anm-1");
+    nav2.classList.remove("slide-out-anm-2");
+    nav2.classList.add("slide-in-anm-2");
+    nav3.classList.remove("slide-out-anm-3");
+    nav3.classList.add("slide-in-anm-3");
+    nav4.classList.remove("slide-out-anm-4");
+    nav4.classList.add("slide-in-anm-4");
+    nav5.classList.remove("slide-out-anm-5");
+    nav5.classList.add("slide-in-anm-5");
   } else {
     // Animate out
     overlay.classList.remove("slide-open");
     overlay.classList.add("slide-close");
+    // Animate out navitem
+    nav1.classList.remove("slide-in-anm-1");
+    nav1.classList.add("slide-out-anm-1");
+    nav2.classList.remove("slide-in-anm-2");
+    nav2.classList.add("slide-out-anm-2");
+    nav3.classList.remove("slide-in-anm-3");
+    nav3.classList.add("slide-out-anm-3");
+    nav4.classList.remove("slide-in-anm-4");
+    nav4.classList.add("slide-out-anm-4");
+    nav5.classList.remove("slide-in-anm-5");
+    nav5.classList.add("slide-out-anm-5");
   }
 }
 
